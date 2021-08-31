ec4f22481072e28e857362fb89130cfb0fe9399c
taehee <wonderfulhuman@naver.com>
2021-08-31 22:51:07 +0900

    Edit: Refactoring totalAmount

--- taehee/src/chapter1/statement.js
+++ taehee/src/chapter1/statement.js
@@ -45,6 +45,8 @@ function usd(anumber) {
 }
 
 function totalVolumeCredits() {
+  // 포인트를 적립한다.
+
   let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
@@ -52,21 +54,26 @@ function totalVolumeCredits() {
   return volumeCredits;
 }
 
+function totalAmount() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
+  }
+
+  return result;
+}
+
 function statement(invoice) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
