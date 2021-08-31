d9b1d5333a90821ff3ed2013e534a8a1ef7595eb
taehee <wonderfulhuman@naver.com>
2021-08-31 16:48:04 +0900

    Edit: amountFor function Defined Location

--- taehee/src/chapter1/statement.js
+++ taehee/src/chapter1/statement.js
@@ -4,6 +4,28 @@ function playFor(perf) {
   return plays[perf.playID];
 }
 
+function amountFor(aPerformance, play) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if(aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return result;
+}
+
 function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -31,27 +53,7 @@ function statement(invoice) {
   return result;
 }
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if(aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-  return result;
-}
+
 
 
 
