846af3919160f2d08e2ef71ca0c8ee6b8b491395
taehee <wonderfulhuman@naver.com>
2021-09-01 03:37:31 +0900

    Edit: Use calculator in enrichPerformance for calculating volumeCredits

--- taehee/src/chapter1/createStatementData.js
+++ taehee/src/chapter1/createStatementData.js
@@ -27,6 +27,14 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 function playFor(perf) {
@@ -38,11 +46,8 @@ function amountFor(aPerformance) {
 }
 
 function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
+  return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+    .volumeCredits;
 }
 
 function enrichPerformance(aPerformance) {
