1dea903529e5a74b2290f74e7b866428d4ed62b8
taehee <wonderfulhuman@naver.com>
2021-09-01 03:51:38 +0900

    Edit: PerformanceCalculator Can't return tragedy amount

--- taehee/src/chapter1/createStatementData.js
+++ taehee/src/chapter1/createStatementData.js
@@ -10,11 +10,7 @@ class PerformanceCalculator {
     let result = 0;
     switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw "오류 발생";
       case "comedy":
         result = 30000;
         if (this.performance.audience > 20) {
@@ -37,7 +33,15 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 
 class ComedyCalculator extends PerformanceCalculator {}
 
