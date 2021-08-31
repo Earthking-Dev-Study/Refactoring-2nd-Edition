7a937f1eebf950480c6522e20a5ac95d04ab35f8
taehee <wonderfulhuman@naver.com>
2021-08-31 22:19:53 +0900

    Rename: format function to use function

--- taehee/src/chapter1/statement.js
+++ taehee/src/chapter1/statement.js
@@ -34,12 +34,12 @@ function volumeCreditsFor(perf) {
   return volumeCredits;
 }
 
-function format(anumber) {
+function usd(anumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format(anumber);
+  }).format(anumber / 100);
 }
 
 function statement(invoice) {
@@ -52,12 +52,12 @@ function statement(invoice) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
