318c3bd390ff4b688abc0bc8e06e2d4fbdf1d55d
taehee <wonderfulhuman@naver.com>
2021-09-01 03:45:28 +0900

    Add: switch statement in  createPerformanceCalculator function

--- taehee/src/chapter1/createStatementData.js
+++ taehee/src/chapter1/createStatementData.js
@@ -37,7 +37,19 @@ class PerformanceCalculator {
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${this.play.type}`);
+  }
   return new PerformanceCalculator(aPerformance, aPlay);
 }
 
