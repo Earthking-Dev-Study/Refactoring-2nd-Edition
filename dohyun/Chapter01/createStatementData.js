0dd03cdfebcde1c89f024d998f7fa2bbe0f0fb8c
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-01 15:45:22 +0900

    amoutFor() 함수를 클래스의 메서드로 이동

--- dohyun/Chapter01/createStatementData.js
+++ dohyun/Chapter01/createStatementData.js
@@ -23,8 +23,8 @@ export default function createStatementData(invoice, plays) {
       playFor(aPerformance)
     );
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행 코드
-    result.play = playFor(result);
-    result.amount = amountFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
@@ -37,29 +37,6 @@ export default function createStatementData(invoice, plays) {
     return result;
   }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -69,5 +46,28 @@ class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performances = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performances.audience > 30) {
+          result += 1000 * (this.performances.audience - 30);
+        }
+        break;
+
+      case "comedy":
+        result = 30000;
+        if (this.performances.audience > 20) {
+          result += 10000 + 500 * (this.performances.audience - 20);
+        }
+        result += 300 * this.performances.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
   }
 }
