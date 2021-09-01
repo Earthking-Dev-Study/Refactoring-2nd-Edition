f700752f28233427a6981e65138e9658ebcbd212
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-01 13:23:08 +0900

    amountFor() 값을 중간데이터로 이동

--- dohyun/Chapter01/AfterRefactoring.js
+++ dohyun/Chapter01/AfterRefactoring.js
@@ -10,6 +10,30 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행 코드
     result.play = playFor(result);
+    result.amount = amountFor(result);
+    return result;
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
     return result;
   }
 
@@ -23,9 +47,7 @@ function renderPlainText(data) {
 
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -35,7 +57,7 @@ function renderPlainText(data) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -61,29 +83,6 @@ function renderPlainText(data) {
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
     return result;
   }
 }
