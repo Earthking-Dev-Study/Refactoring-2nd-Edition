4bfcb8eda92fbb323a200a833991c9db9c0d45b2
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-01 16:04:49 +0900

    적립포인트 메서드 다른 부분이 있는 comedy 로직만 해당 서브 클래스로 내림

--- dohyun/Chapter01/createStatementData.js
+++ dohyun/Chapter01/createStatementData.js
@@ -58,11 +58,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performances.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performances.audience / 5);
-    return result;
+    return Math.max(this.performances.audience - 30, 0);
   }
 }
 
@@ -84,5 +80,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performances.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performances.audience / 5);
   }
 }
