b0e64953ee009261ef3597e66ddc6cfb4b73204b
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-01 11:13:07 +0900

    plays 매개변수 삭제

--- dohyun/Chapter01/AfterRefactoring.js
+++ dohyun/Chapter01/AfterRefactoring.js
@@ -18,7 +18,7 @@ function statement(invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
