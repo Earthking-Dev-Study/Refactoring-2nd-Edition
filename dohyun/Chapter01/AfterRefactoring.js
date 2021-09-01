b250a64a0c97648d37cca2f817d80412e654766e
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-01 09:53:22 +0900

    volumeCredits변수를 반복문쪼개기, 문장슬라이드하기, 함수추출하기, 변수 인라인하기

--- dohyun/Chapter01/AfterRefactoring.js
+++ dohyun/Chapter01/AfterRefactoring.js
@@ -3,12 +3,9 @@ import invoices from "./invoices.json";
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
@@ -17,8 +14,16 @@ function statement(invoice, plays) {
   }
 
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
