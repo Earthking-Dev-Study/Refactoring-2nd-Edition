a4c8be49e01fd0586f1885d2969f8ee611a9e049
taehee <wonderfulhuman@naver.com>
2021-09-19 08:57:27 +0900

    테스트끼리 상호작용하게 하는 공유 픽스쳐를 생성하는 원인이다. 즉, 이렇게 asia를 따로 빼면안된다.

--- taehee/src/chapter4/Province.js
+++ taehee/src/chapter4/Province.js
@@ -50,7 +50,7 @@ class Province {
 
   // 생산 부족분 계산
   get shortfall() {
-    return this._demand - this.totalProduction * 2;
+    return this._demand - this.totalProduction;
   }
 
   // 수익 계산
--- taehee/test/chapter4/province.spec.js
+++ taehee/test/chapter4/province.spec.js
@@ -2,8 +2,13 @@ const { Province } = require('../../src/chapter4/Province');
 const { sampleProvinceData } = require('../../src/chapter4/Province');
 
 describe('province', function () {
+  const asia = new Province(sampleProvinceData());
+
   it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
+  });
+
+  it('profit', function () {
+    expect(asia.profit).toEqual(230);
   });
 });
