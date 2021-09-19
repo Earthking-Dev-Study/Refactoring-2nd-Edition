75d1429c940f2e072a886703cd6e7654b5b4ac6d
taehee <wonderfulhuman@naver.com>
2021-09-19 09:17:43 +0900

    Test: No Producers

--- taehee/test/chapter4/province.spec.js
+++ taehee/test/chapter4/province.spec.js
@@ -2,7 +2,10 @@ const { Province } = require('../../src/chapter4/Province');
 const { sampleProvinceData } = require('../../src/chapter4/Province');
 
 describe('province', function () {
-  const asia = new Province(sampleProvinceData());
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
 
   it('shortfall', function () {
     expect(asia.shortfall).toEqual(5);
@@ -10,5 +13,32 @@ describe('province', function () {
 
   it('profit', function () {
     expect(asia.profit).toEqual(230);
+  });
+
+  it('change production', function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
+  });
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: 'No Producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it('shortfall', function () {
+    expect(noProducers.shortfall).toEqual(30);
+  });
+
+  it('profit', function () {
+    expect(noProducers.profit).toEqual(0);
   });
 });
