3130ad630ac7b588a284e2405d795566024a65de
이도현 <ksmfou98@naver.com>
2021-09-22 17:52:54 +0900

    Test : empty strin demand

--- dohyun/Chapter04/src/test/Province.test.js
+++ dohyun/Chapter04/src/test/Province.test.js
@@ -34,6 +34,12 @@ describe("province", function () {
     expect(asia.shortfall).equal(-26);
     expect(asia.profit).equal(-10);
   });
+
+  it("empty string demand", function () {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
 });
 
 describe("no producers", function () {
