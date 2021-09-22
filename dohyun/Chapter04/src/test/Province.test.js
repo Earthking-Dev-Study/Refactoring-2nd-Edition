3625ed98fb81e1655697f83d17406cbe1a197783
이도현 <ksmfou98@naver.com>
2021-09-22 17:44:36 +0900

    Test : change production

--- dohyun/Chapter04/src/test/Province.test.js
+++ dohyun/Chapter04/src/test/Province.test.js
@@ -16,4 +16,10 @@ describe("province", function () {
   it("profit", function () {
     expect(asia.profit).equal(230);
   });
+
+  it("change production", function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });
 });
