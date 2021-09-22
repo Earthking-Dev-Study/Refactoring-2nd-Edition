795e2f64f203a50848f1cae2da487edecedfdc4b
이도현 <ksmfou98@naver.com>
2021-09-22 17:54:36 +0900

    Test : string for producers

--- dohyun/Chapter04/src/test/Province.test.js
+++ dohyun/Chapter04/src/test/Province.test.js
@@ -62,3 +62,16 @@ describe("no producers", function () {
     expect(noProducers.profit).equal(0);
   });
 });
+
+// describe("string for producers", function () {
+//   it("", function () {
+//     const data = {
+//       name: "String producers",
+//       producers: "",
+//       demand: 30,
+//       price: 20,
+//     };
+//     const prov = new Province(data);
+//     expect(prov.shortfall).equal(0);
+//   });
+// });
